894ce2c4a60ad710d8f5628297fd49ee367386f1
JiekRuan <ruanjiek2001@gmail.com>
2022-01-05 16:43:59 +0100

    finition du js

---
+++ js/accueil_app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("chargé");
+    let submit_log = localStorage.getItem("user");
+    let el = document.querySelector(".email");
+    let el2 = document.querySelector(".mail");
+    el.innerText += ` ${submit_log}`;
+    el2.innerText += ` ${submit_log}`;
+    let btn = document.querySelector(".unlocked");
+    /*event*/
+    btn.addEventListener("click", () => {
+        localStorage.clear();
+        sessionStorage.clear();
+        document.location.assign("tendances.html");
+    });
+});
--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    let errormsg = document.querySelector('.warning');
-    console.log("hello");
-    if(input = ""){ 
-        errormsg.innerText += `Toutes les zones doivent être remplis`;
-    }
-       
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("chargé");
+    let btn, form, warning, email, password, submit;
+    /* portée des varibles globales*/
+    warning = document.querySelector(".warning");
     
-})
+    let expr = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/;
+    btn = document.getElementsByClassName("btn");
+    form = document.getElementsByClassName("form")[0];
+    // btn.addEventListener("click", e => {
+    //     e.preventDefault();
+    //     form.classList.add("appear");
+    // });
+    /* methode */
+    let formActive = () => {
+        /* portée locale */
+        email = document.getElementsByTagName("input")[0].value;
+        password = document.getElementsByTagName("input")[1].value;
+        if (email.match(expr) && password) {
+            warning.classList.add("success");
+            warning.style.display = "block";
+            warning.innerText = "";
+            localStorage.setItem("user", email); //stocker le login
+            sessionStorage.setItem("user", "sessionid");
+            document.location.assign("tendances.html");
+        } else if (!email.match(expr)) {
+            warning.style.display = "block";
+            warning.innerText = "Mail ou mot de passe incorrect";
+            warning.classList.remove("success");
+            warning.classList.add("error")
+        }
+        else if (!password.match(expr)) {
+            warning.style.display = "block";
+            warning.innerText = "Mot de passe incorrect";
+            warning.classList.remove("success");
+            warning.classList.add("error")
+        } else {
+            warning.style.display = "block";
+            warning.innerText = "remplir les champs";
+        }
+    };
+    /*appelle de la fonction*/
+    submit = document.getElementsByClassName("btn");
+    /*bouton valider*/
+    submit.addEventListener("click", e => {
+        e.preventDefault(); //empeche rechargement de la page
+        formActive();
+    });
+});
